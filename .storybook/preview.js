bd1a6e6b1fc64918bc32f58f3c1e40989a7b379f
Adam Bulmer <adam.bulmer@moonpig.com>
2022-07-22 15:00:40 +0100

    bug reproduced

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { DocsContainer } from "@storybook/addon-docs";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +8,30 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+  docs: {
+    container: ({ context, children }) => {
+      const { theme } = context.globals;
+
+      console.log("Context Object Stuck", context);
+      console.log("theme value stuck ---->", theme);
+
+      return <DocsContainer context={context}>{children}</DocsContainer>;
+    },
+  },
+};
+
+export const globalTypes = {
+  theme: {
+    name: "Brand",
+    description: "Theme selector",
+    defaultValue: "default_item",
+    toolbar: {
+      icon: "eye",
+      items: [
+        { value: "default_item", title: "Default Item" },
+        { value: "item_two", title: "Item Two" },
+      ],
+      showName: true,
+    },
+  },
+};
